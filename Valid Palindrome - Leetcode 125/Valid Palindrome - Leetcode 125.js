e8ac4c82eafe1926d83839215d8c8fb3bdc357b9
Xuan Tho <tholx2811workspace@gmail.com>
2024-08-21 10:43:15 +0700

    Optimized Palindrome

--- Valid Palindrome - Leetcode 125/Valid Palindrome - Leetcode 125.js
+++ Valid Palindrome - Leetcode 125/Valid Palindrome - Leetcode 125.js
@@ -1,4 +1,4 @@
-// Solution 1: O(n)
+// Solution 1:  Time: O(n), Space: O(n)
 const isPalindrome = function (s) {
   const filter = s.match(/[a-zA-Z0-9]/g);
 
@@ -11,4 +11,28 @@ const isPalindrome = function (s) {
   if (str === reversedStr) return true;
 
   return false;
+};
+
+// Solution 2: Time: O(n) , Space: O(1)
+const isPalindrome = function (s) {
+  let l = 0,
+    r = s.length - 1;
+
+  while (l < r) {
+    if (!s[l].match(/^[a-z0-9]+$/i)) {
+      l++;
+      continue;
+    }
+    if (!s[r].match(/^[a-z0-9]+$/i)) {
+      r--;
+      continue;
+    }
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+      return false;
+    }
+
+    l++;
+    r--;
+  }
+  return true;
 };
