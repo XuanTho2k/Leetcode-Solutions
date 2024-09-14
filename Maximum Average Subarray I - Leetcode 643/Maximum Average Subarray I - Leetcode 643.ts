afc47debf64baef708d30edeacda05213e793ffb
Xuan Tho <tholx2811workspace@gmail.com>
2024-09-14 20:05:04 +0700

    Added Maximum Average Subarray I

---
+++ Maximum Average Subarray I - Leetcode 643/Maximum Average Subarray I - Leetcode 643.ts
@@ -0,0 +1,22 @@
+// Solution 1: T - O(n), S - O(1)
+const findMaxAverage = (nums: number[], k: number): number => {
+  const n = nums.length;
+  let cur_sum = 0;
+
+  for (let i = 0; i < k; i++) {
+   cur_sum+=nums[i] 
+  }
+
+  let max_avg = cur_sum/k
+
+  for (let i = ; i < n; i++) {
+    cur_sum += nums[i]
+    cur_sum -= nums[i-k]
+
+    max_avg = Math.max(max_avg, cur_sum/k)
+    
+  }
+
+  return max_avg
+
+};
