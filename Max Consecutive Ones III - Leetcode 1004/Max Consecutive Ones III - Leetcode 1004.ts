a58e675710c1dfc27afd83f3c85dbe52b38fd97a
Xuan Tho <tholx2811workspace@gmail.com>
2024-09-16 10:01:35 +0700

    Max Consecutive Ones III

---
+++ Max Consecutive Ones III - Leetcode 1004/Max Consecutive Ones III - Leetcode 1004.ts
@@ -0,0 +1,18 @@
+function longestOne(nums: number[], k: number): number {
+  const n = nums.length;
+  let [w_max, w_start, nums_zero] = [0, 0, 0];
+
+  for (let w_end = 0; w_end < n; w_end++) {
+    if (nums[w_end] === 0) {
+      nums_zero++;
+    }
+
+    while (nums_zero > k) {
+      if (nums[w_start] === 0) nums_zero--;
+      w_start++;
+    }
+
+    w_max = Math.max(w_max, w_end - w_start + 1);
+  }
+  return 0;
+}
