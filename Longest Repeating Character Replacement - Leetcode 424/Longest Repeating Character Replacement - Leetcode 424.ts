124c9238e079bd74b59bdccce17f323828f0c860
Xuan Tho <tholx2811workspace@gmail.com>
2024-09-23 14:55:45 +0700

    Added Longest Repeating Character Replacement

--- Longest Repeating Character Replacement - Leetcode 424/Longest Repeating Character Replacement - Leetcode 424.ts
+++ Longest Repeating Character Replacement - Leetcode 424/Longest Repeating Character Replacement - Leetcode 424.ts
@@ -1,18 +1,18 @@
 const characterReplayment = (s: string, k: number): number => {
   const n = s.length;
-  const counts = Array(26).fill(0);
-  let [left, ans, max_count] = [0, 0, 0];
+  const m = Array(26).fill(0);
+  let [left, longest, max_count] = [0, 0, 0];
 
   for (let right = 0; right < n; right++) {
-    max_count = Math.max(max_count, ++counts[s.charCodeAt(right) - 65]);
+    max_count = Math.max(max_count, ++m[s.charCodeAt(right) - 65]);
 
     while (right - left + 1 - max_count > k) {
-      counts[s.charCodeAt(left) - 65]--;
+      m[s.charCodeAt(left) - 65]--;
       left++;
     }
 
-    ans = Math.max(ans, right - left + 1);
+    longest = Math.max(longest, right - left + 1);
   }
 
-  return ans;
+  return longest;
 };
