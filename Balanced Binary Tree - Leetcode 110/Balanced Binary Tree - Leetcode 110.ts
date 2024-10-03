7446f34ddaf87b6788250038789a9d9cc75d77a6
Xuan Tho <tholx2811workspace@gmail.com>
2024-10-03 11:16:07 +0700

    Added Balanced Binary Tree

---
+++ Balanced Binary Tree - Leetcode 110/Balanced Binary Tree - Leetcode 110.ts
@@ -0,0 +1,24 @@
+import { TreeNode } from "../Datastructure/TreeNode";
+class Solution {
+  isBalanced(root: TreeNode | null): boolean {
+    let ans = [true];
+
+    this.calculateHeight(root, ans);
+
+    return ans[0];
+  }
+
+  calculateHeight(node: TreeNode | null, ans: boolean[]): number {
+    if (node === null) return 0;
+
+    const left_height = this.calculateHeight(node.left, ans);
+    const right_height = this.calculateHeight(node.right, ans);
+
+    if (Math.abs(left_height - right_height) > 1) {
+      ans[0] = false;
+      return 0;
+    }
+
+    return 1 + Math.max(left_height, right_height);
+  }
+}
