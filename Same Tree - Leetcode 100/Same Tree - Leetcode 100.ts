6330b79572864a5fc525986504e1b9e88e337f8f
Xuan Tho <tholx2811workspace@gmail.com>
2024-10-03 14:13:54 +0700

    Added Same Tree

---
+++ Same Tree - Leetcode 100/Same Tree - Leetcode 100.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from "../DataStructure/TreeNode";
+
+class Solution {
+  isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    return this.balanced(p, q);
+  }
+
+  balanced(n: TreeNode | null, m: TreeNode | null): boolean {
+    if (n === null && m === null) return true;
+    if ((n === null && m !== null) || (n !== null && m === null)) return true;
+    if (n?.val !== m?.val) return false;
+
+    return (
+      this.balanced(n?.left!, m?.left!) && this.balanced(n?.right!, m?.right!)
+    );
+  }
+}
